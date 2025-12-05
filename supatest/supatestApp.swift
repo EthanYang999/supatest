3c2ebc9817fa4604e05d2d2b3950be2aadd5aa60
EthanYang999 <ethan.zt.yang@gmail.com>
2025-12-05 14:19:24 +0800

    Initial Commit

---
+++ supatest/supatestApp.swift
@@ -0,0 +1,17 @@
+//
+//  supatestApp.swift
+//  supatest
+//
+//  Created by Ethan on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct supatestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ supatestTests/supatestTests.swift
@@ -0,0 +1,16 @@
+//
+//  supatestTests.swift
+//  supatestTests
+//
+//  Created by Ethan on 12/5/25.
+//
+
+import Testing
+
+struct supatestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
